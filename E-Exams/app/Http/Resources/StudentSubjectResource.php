5cee0d5fb7cb26c536bfde19de826b86cb907361
ahmedhegazee <ahmedhegazee@hotmail.com>
2020-03-22 21:50:41 +0200

    adding passed and failed exams , wrong and correct answers to StudentSubjectResource

--- E-Exams/app/Http/Resources/StudentSubjectResource.php
+++ E-Exams/app/Http/Resources/StudentSubjectResource.php
@@ -14,6 +14,21 @@ class StudentSubjectResource extends JsonResource
      */
     public function toArray($request)
     {
+        $exams=auth()->user()->student->trainingExams()->subject($this->id)->get();
+        $passedExams =$exams->filter(function ($exam){
+                if($exam->result->marks>=50)
+                    return $exam;
+            })->count();
+        $failedExams=$exams->filter(function ($exam){
+            if($exam->result->marks<50)
+                return $exam;
+        })->count();
+        $correctAnswers=$exams->map(function($exam){
+            return $exam->result->correct_answers;
+        })->sum();
+        $wrongAnswers=$exams->map(function($exam){
+            return $exam->result->wrong_answers;
+        })->sum();
         return [
             'id'=>$this->id,
             'name'=>$this->subject_name,
@@ -23,6 +38,10 @@ class StudentSubjectResource extends JsonResource
             'questions'=>$this->chapters->map(function ($chapter){
                 return $chapter->questions->count();
             })->sum(),
+            'pass_exams'=>$passedExams,
+            'failed_exams'=>$failedExams,
+            'correct_answers'=>$correctAnswers,
+            'wrong_answers'=>$wrongAnswers,
         ];
     }
 }
